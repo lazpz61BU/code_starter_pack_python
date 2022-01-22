2fffd2fdb80443f60f02f9a55705fd17c258ee30
Lazaro Perez <lazpz61@bu.edu>
2022-01-22 15:13:06 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+from flask_heroku import Heroku
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+
+
