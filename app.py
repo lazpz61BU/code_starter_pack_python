a1c77b4b1ea9e76d04f5ed639fac6314e6413d79
Lazaro Perez <lazpz61@bu.edu>
2022-01-26 01:32:25 -0500

    Fixing bugs

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
 
 db = SQLAlchemy(app)
-ma = Marshhmallow(app)
+ma = Marshmallow(app)
 
 # for Hosting
 heroku = Heroku(app)
